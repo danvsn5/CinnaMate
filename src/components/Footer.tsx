34bd79b7e959b828b0fcbf97a9956987bacd2091
danvsn5 <danvsn5@gmail.com>
2024-04-30 13:49:33 +1200

    feat: added footer and links to git repo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,39 @@
+import tmdb from '../resources/images/tmdb.svg'
+import github from '../resources/images/github.svg'
+
 const Footer = () => {
   return (
-    <div>
-        <h1>Footer</h1>
-    </div>
+
+    <footer>
+
+      <div className="footer-bar">
+        <div className="attribution">
+          <div className="tmdb-logo">
+            <a href="https://www.themoviedb.org/" target='_blank'>
+            <img src={tmdb} alt="TMDB" />
+            </a>
+          </div>
+          <div className="text-attribution">
+            <h6>CinnaMate uses TMDB and the TMDB APIs but is not endorsed, certified or otherwise approved by TMBD</h6>
+          </div>
+        </div>
+
+        <div className="github">
+          <div className="github-logo">
+            <img src={github} alt="GitHub" />
+          </div>
+          <div className='github-referral'>
+          <h6>See my GitHub profile and other repositories at 
+            <a href="https://github.com/danvsn5" className='git-link' target='_blank'> github.com/danvsn5</a>
+          </h6>
+          </div>
+        </div>
+        <div className="footer-title"><h1>CinnaMate</h1></div>
+
+      </div>
+
+    </footer>
+
   )
 }
 
