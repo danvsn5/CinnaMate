f682428c78d201ec9c34c9c87552df254c43507c
danvsn5 <danvsn5@gmail.com>
2024-05-01 12:09:51 +1200

    feat: implemented api fetch call for gathering popular movies playing this week and displays them with list on main page

--- src/components/PageHome.tsx
+++ src/components/PageHome.tsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
 import Subheader from "./Subheader"
 
 const PageHome = () => {
-  return (
 
-    <Subheader subheaderTitle="See What's Trending" />
+  let [results, setResults] = useState([]);
+
+  const url = 'https://api.themoviedb.org/3/trending/movie/week?language=en-US';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWIxNjYwNWY5MzQ5MzJiYzk4N2NlZTJjYjRiZGMwYyIsInN1YiI6IjY2MmNmMTk1ZTMzZjgzMDEyMjIxMDc3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xu5UsO6VS90CsemI07dMUU6xjKcbTxQ1UrfGjC9iFjc'
+    }
+  };
+
+  fetch(url, options)
+    .then((res: any) => res.json())
+    .then((data: any) => {
+      console.log(data);
+      setResults(data.results);
+    })
+    .catch((err: any) => console.error('error:' + err));
+
+    const movieTitles = results.map((movie:any) =>
+      (
+        <li>{movie.title}</li>
+      )
+      )
+
+  return (
+    <div>
+      <Subheader subheaderTitle="See What's Trending" />
+      
+      <h2>{movieTitles}</h2>
+
+    </div>
 
   )
 }
