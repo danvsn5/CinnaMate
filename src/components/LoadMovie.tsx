1e6e21d1774b26ef8daeb5dc289760bc310ad059
danvsn5 <danvsn5@gmail.com>
2024-05-03 10:21:57 +1200

    fix: fixed broken code for movie data not  loading and managed to get it working for *genre* only. REASONING: API call collects trending movies as an ARRAY of *results*; API call for individual movies do not contain any such array, thus no mapping could occur and hence the TypeError

--- src/components/LoadMovie.tsx
+++ src/components/LoadMovie.tsx
@@ -2,11 +2,15 @@ import { useState } from 'react';
 
 const LoadMovie = ({movie}:any) => {
 
+  
+    // fetches API data for trending movies this week and stores data inside results array
+  
+  
   // fetches API data for trending movies this week and stores data inside results array
 
-  let [results, setResults] = useState([]);
+  let [genres, setResults] = useState([]);
 
-  const url = `https://api.themoviedb.org/3/movie/693134?language=en-US`;
+  const url = `https://api.themoviedb.org/3/movie/${movie.id}?language=en-US`;
   const options = {
     method: 'GET',
     headers: {
@@ -17,30 +21,32 @@ const LoadMovie = ({movie}:any) => {
 
   // if the call was succcesful then store the data gathered inside the results array
 
+  if(genres.length ==0){
     fetch(url, options)
       .then((res: any) => res.json())
       .then((data: any) => {
         console.log(data);
-        setResults(data.results);
+        setResults(data.genres);
       })
       .catch((err: any) => console.error('error:' + err));
+    }
 
-      let movieitems = results?.map((mov: any) =>
-        (
-          <li key={mov.id} className="trending-list">
-            <h6>{mov.budget}</h6>
-            <h6>{mov.original_title}</h6>
-            <h6>{mov.popularity}</h6>
-            <h6>{mov.budget}</h6>
-          </li>
-        )
-        )
 
+  // movieCards element maps the collected elements from the array collected
+  // by the API and is used in return code through <Card> element
+  let movieCards = genres.map((movie: any) =>
+  (
+    <li key={movie.id} className="trending-list">
+        <h6>{movie.id}</h6>
+        <h6>{movie.name}</h6>
+    </li>
+  )
+  )
 
   return (
     <div>
         <h6>Trying to load movie tester</h6>
-        {movieitems}
+        {movieCards}
     </div>
   )
 }
