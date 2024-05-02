157f0923ee3f1ea8b92d5a05191cba0f5471e2f9
danvsn5 <danvsn5@gmail.com>
2024-05-02 21:16:07 +1200

    feat: added linkedin link and profile in footer bar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import tmdb from '../resources/images/tmdb.svg'
 import github from '../resources/images/github.svg'
+import linkedin from '../resources/images/linkedin.svg'
 
 const Footer = () => {
   return (
@@ -18,16 +19,26 @@ const Footer = () => {
               <h6>CinnaMate uses TMDB and the TMDB APIs but is not endorsed, certified or otherwise approved by TMBD</h6>
             </div>
           </div>
-
           <div className="github">
+
+
             <div className="github-logo">
+              
               <a href="https://github.com/danvsn5" className='git-link' target='_blank'>
-                <img src={github} alt="GitHub" />
+                <img className='logo-img' src={github} alt="GitHub" />
               </a>
+
+              <a href="https://www.linkedin.com/in/daniilv522/" className='git-link' target='_blank'>
+                <img className='logo-img' src={linkedin} alt="LinkedIn" />
+              </a>
+
             </div>
+
+
             <div className='github-referral'>
-              <h6>See my GitHub profile and other repositories at
+              <h6>See more about me in my GitHub profile and my LinkedIn
                 <a href="https://github.com/danvsn5" className='git-link' target='_blank'> github.com/danvsn5</a>
+                <a href="https://www.linkedin.com/in/daniilv522/" className='git-link' target='_blank'> linkedin.com/daniilv522</a>
               </h6>
             </div>
           </div>
