21a1eda1bda169fe7a13b36601625dcef69b1599
danvsn5 <danvsn5@gmail.com>
2024-05-04 23:30:00 +1200

    feat: added formatting for search bar in browsing tab and changes button styles across site to improve matching of the site's theme

--- src/components/PageBrowser.tsx
+++ src/components/PageBrowser.tsx
@@ -1,11 +1,16 @@
 import Subheader from "./Subheader"
+import SearchInput from "./browseMovies/SearchInput"
 
 const PageBrowser = () => {
   return (
 
     <div className="main-content">
       <Subheader subheaderTitle="Browse Movies" />
+      <div className="browsing-content">
+        <SearchInput />
+      </div>
     </div>
+
   )
 }
 
--- src/components/browseMovies/SearchInput.tsx
+++ src/components/browseMovies/SearchInput.tsx
@@ -1,9 +1,12 @@
 const SearchInput = () => {
-  return (
-    <div className="search-input-wrapper">
-
-    </div>
-  )
+    return (
+        <div className="search-input-wrapper">
+            <input className="search-input-editor" type="text" placeholder="Browse for Movies..."/>
+            <button className="search-input-button">
+                <i className="icon fa-solid fa-magnifying-glass"></i>
+            </button>
+        </div>
+    )
 }
 
 export default SearchInput
