74d4f737871660702974b39531cc431b95b89111
danvsn5 <danvsn5@gmail.com>
2024-05-05 12:48:52 +1200

    feat: implemented useState hook ijn searhcInput.tsx to constantly collect the current input value from the search bar an alert as testing functionality

--- src/components/browseMovies/SearchInput.tsx
+++ src/components/browseMovies/SearchInput.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react"
+
 const SearchInput = () => {
 
-// MAIN FUNCTIONALITY
-// ---
-// upon loading the page, browsing-sub-sub title will display trending movies at that time 
-// upon inputting a string inside the text editor (maybe) a short list of 2-3 movies will show
-// underneath the search bar; to view full list of movies, the user clicks the search button,
-// causing the browsing-sub-sub title to change to whatever the input string was (reformatted)
-// and the cards to display like how they are displayed in the home trending page.  
+    // MAIN FUNCTIONALITY
+    // ---
+    // upon loading the page, browsing-sub-sub title will display trending movies at that time 
+    // upon inputting a string inside the text editor (maybe) a short list of 2-3 movies will show
+    // underneath the search bar; to view full list of movies, the user clicks the search button,
+    // causing the browsing-sub-sub title to change to whatever the input string was (reformatted)
+    // and the cards to display like how they are displayed in the home trending page.  
 
+    let [inputValue, setInputValue] = useState("")
+
+    const inputChange = (e: any) => {
+        setInputValue(e.currentTarget.value)
+    }
+
+    const searchClick = (e:any) => {
+        alert(inputValue)
+    }
 
     return (
         <div className="search-input-wrapper">
-            <input className="search-input-editor" type="text" placeholder="Browse for Movies..."/>
-            <button className="search-input-button">
+            <input className="search-input-editor" type="text" placeholder="Browse for Movies..." onChange={inputChange} />
+            <button className="search-input-button" onClick={searchClick}>
                 <i className="icon fa-solid fa-magnifying-glass"></i>
             </button>
         </div>
