21439d03d298636ed7eead9f97fc1d95a27150e8
danvsn5 <danvsn5@gmail.com>
2024-05-05 20:58:34 +1200

    feat: when hovering over the thumbnail or the title, both elements get highlighted with newly learnt method while working on browsing page

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -5,13 +5,37 @@ import { Link } from "react-router-dom"
 // card wrapper that will contain the wrapped elements for each movie
 // including title, thumbnail, date released (smaller font) and buttons for adding to MyMovies
 const Card = ({ movie }: any) => {
+
+  const id1: any = movie.id - 1000000;
+
+
+  const setHoverChangeThumb = () => {
+    let id: any = movie.id;
+    const thumb = document.getElementById(id)
+    if (thumb) thumb.style.boxShadow = "0px 0px 16px 8px var(--light-purple)";
+    const title = document.getElementById(id1)
+    if (title) title.style.color = "var(--light-purple)";
+    if (title) title.style.textDecoration = "underline";
+
+  }
+
+  const setHoverChangeOffThumb = () => {
+    let id: any = movie.id;
+    const thumb = document.getElementById(id)
+    if (thumb) thumb.style.boxShadow = "0px 0px 0px 3px var(--dark-purple)";
+    const title = document.getElementById(id1)
+    if (title) title.style.color = "#ffffff";
+    if (title) title.style.textDecoration = "none";
+
+  }
+
   return (
 
     <div className="page-card">
       <Thumbnail movie={movie} />
       <AddButtons />
-      <Link to='/yourmovie' state={{movie}}>
-      <h2 className="thumbnail-title">{movie.title}</h2>
+      <Link to='/yourmovie' state={{ movie }}>
+        <h2 id={id1} onMouseOver={setHoverChangeThumb} onMouseLeave={setHoverChangeOffThumb} className="thumbnail-title">{movie.title}</h2>
       </Link>
       <h2 className="thumbnail-date">{movie.release_date.substring(0, 4)}</h2>
     </div>
--- src/components/cards/Thumbnail.tsx
+++ src/components/cards/Thumbnail.tsx
@@ -4,12 +4,33 @@ import { Link } from "react-router-dom";
 // thumbnail element used within cards to be displayed to user
 const Thumbnail = ({ movie }: any) => {
 
+  const id1: any = movie.id -1000000;
+
+  const setHoverChangeThumb = () => {
+    let id: any = movie.id;
+    const thumb = document.getElementById(id)
+    if (thumb) thumb.style.boxShadow = "0px 0px 16px 8px var(--light-purple)";
+    const title = document.getElementById(id1)
+    if (title) title.style.color = "var(--light-purple)";
+    if (title) title.style.textDecoration = "underline";
+}
+
+const setHoverChangeOffThumb = () => {
+  let id: any = movie.id;
+  const thumb = document.getElementById(id)
+  if (thumb) thumb.style.boxShadow = "0px 0px 0px 3px var(--dark-purple)";
+  const title = document.getElementById(id1)
+    if (title) title.style.color = "#ffffff";
+    if (title) title.style.textDecoration = "none";
+
+}
+
   return (
     <div className="card-thumbnail">
       {movie.poster_path ? (
 
-        <Link to='/yourmovie' state={{movie}}>
-        <img className="actual-thumb" src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`} alt={`${movie.title}`} />
+        <Link to='/yourmovie' state={{ movie }}>
+          <img id={movie.id} className="actual-thumb" onMouseOver={setHoverChangeThumb} onMouseLeave={setHoverChangeOffThumb} src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`} alt={`${movie.title}`} />
         </Link>
       ) : (
         <div className="poster-not-found"></div>
