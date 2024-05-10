2280bd77fb0a96cb8af97711fecde313f611d2ab
danvsn5 <danvsn5@gmail.com>
2024-05-10 20:40:26 +1200

    feat, refactor: prequery is created specifically for the search wrapper below the input box that updates depending on the user's input; refactored code in searchInput.tsx file

--- src/components/browseMovies/SearchInput.tsx
+++ src/components/browseMovies/SearchInput.tsx
@@ -15,30 +15,13 @@ const SearchInput = () => {
     // causing the browsing-sub-sub title to change to whatever the input string was (reformatted)
     // and the cards to display like how they are displayed in the home trending page.  
 
+
+    /* —————————————————————————————————————————————————————————————————————————————————————————————— */
+    /*                                       Filter and Mapping                                       */
+    /* —————————————————————————————————————————————————————————————————————————————————————————————— */
+
     let [results, setResults] = useState([]);
-
-    const urlTrending = 'https://api.themoviedb.org/3/trending/movie/week?language=en-US';
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWIxNjYwNWY5MzQ5MzJiYzk4N2NlZTJjYjRiZGMwY'
-                + 'yIsInN1YiI6IjY2MmNmMTk1ZTMzZjgzMDEyMjIxMDc3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xu'
-                + '5UsO6VS90CsemI07dMUU6xjKcbTxQ1UrfGjC9iFjc'
-        }
-    };
-
-    // if the call was succcesful then store the data gathered inside the results array
-    if (results.length == 0) {
-        fetch(urlTrending, options)
-            .then((res: any) => res.json())
-            .then((data: any) => {
-                setResults(data.results);
-            })
-            .catch((err: any) => console.error('error:' + err));
-
-    }
-
+    let [searchCardResults, setSearchCardResults] = useState([]);
 
     // filters movies so that if they either do not have a poster, backdrop or overview, they will not be
     // displayed after the search
@@ -59,7 +42,7 @@ const SearchInput = () => {
 
     ))
 
-    let searchMovieCards = results.filter(movieFilter).slice(0, 3).map((movie: movieType) =>
+    let searchMovieCards = searchCardResults.filter(movieFilter).slice(0, 3).map((movie: movieType) =>
     (
         <li key={movie.id} className="search-card-list">
             <Link to='/yourmovie' state={{ movie }}>
@@ -68,13 +51,15 @@ const SearchInput = () => {
         </li>
     ))
 
-    /* -------------------------- - ------------------------- */
+    /* —————————————————————————————————————————————— - ————————————————————————————————————————————— */
 
 
     let [inputValue, setInputValue] = useState("")
 
+    // as input changes, a query is sent out and maps to the three cards that are displayed as a result from the input query
     const inputChange = (e: any) => {
         setInputValue(e.currentTarget.value)
+        getSpecificSmallQuery()
     }
 
     const Search = () => {
@@ -108,6 +93,42 @@ const SearchInput = () => {
     // 'main content'ish and then port the logic for initial trendiing query here; once the user inputs
     // a new query, the cards update based on the user's input query
 
+    const setTextChange = () => {
+
+        const text = document.getElementById("subheader-title-inner-content")
+        let initStr = "Searching for: "
+        if (text) text.innerHTML = initStr.concat({ inputValue }.inputValue);
+
+    }
+
+    /* —————————————————————————————————————————————————————————————————————————————————————————————— */
+    /*                                           Query Calls                                          */
+    /* —————————————————————————————————————————————————————————————————————————————————————————————— */
+
+    const urlTrending = 'https://api.themoviedb.org/3/trending/movie/week?language=en-US';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWIxNjYwNWY5MzQ5MzJiYzk4N2NlZTJjYjRiZGMwY'
+                + 'yIsInN1YiI6IjY2MmNmMTk1ZTMzZjgzMDEyMjIxMDc3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xu'
+                + '5UsO6VS90CsemI07dMUU6xjKcbTxQ1UrfGjC9iFjc'
+        }
+    };
+
+    // if the call was succcesful then store the data gathered inside the results array
+    if (results.length == 0) {
+        fetch(urlTrending, options)
+            .then((res: any) => res.json())
+            .then((data: any) => {
+                setResults(data.results);
+            })
+            .catch((err: any) => console.error('error:' + err));
+
+    }
+
+    /* —————————————————————————————————————————————— - ————————————————————————————————————————————— */
+
     const getSpecificQuery = () => {
 
         const urlSpecific = `https://api.themoviedb.org/3/search/movie?query=${inputValue}&include_adult=false&language=en&page=1`;
@@ -126,8 +147,6 @@ const SearchInput = () => {
             .then((res: any) => res.json())
             .then((data: any) => {
                 if (data.total_results != 0) {
-                    // TODO create filter so that only movies WITH a poster path and and a backdrop path
-                    // are returned to reduce the possibility of returning a garbage results
                     setResults([])
                     setResults(data.results);
                 } else {
@@ -140,14 +159,32 @@ const SearchInput = () => {
 
     }
 
-    const setTextChange = () => {
+    const getSpecificSmallQuery = () => {
 
-        const text = document.getElementById("subheader-title-inner-content")
-        let initStr = "Searching for: "
-        if (text) text.innerHTML = initStr.concat({ inputValue }.inputValue);
+        const urlSpecific = `https://api.themoviedb.org/3/search/movie?query=${inputValue}&include_adult=false&language=en&page=1`;
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWIxNjYwNWY5MzQ5MzJiYzk4N2NlZTJjYjRiZGMwY'
+                    + 'yIsInN1YiI6IjY2MmNmMTk1ZTMzZjgzMDEyMjIxMDc3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xu'
+                    + '5UsO6VS90CsemI07dMUU6xjKcbTxQ1UrfGjC9iFjc'
+            }
+        };
 
+        // if the call was succcesful then store the data gathered inside the results array
+        fetch(urlSpecific, options)
+            .then((res: any) => res.json())
+            .then((data: any) => {
+                if (data.total_results != 0) {
+                    setSearchCardResults([])
+                    setSearchCardResults(data.results);
+                }
+            })
+            .catch((err: any) => console.error('error:' + err));
     }
 
+    /* —————————————————————————————————————————————— - ————————————————————————————————————————————— */
 
     return (
         <div className=" total-container">
