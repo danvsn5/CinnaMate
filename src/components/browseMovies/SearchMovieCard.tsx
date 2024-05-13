bdafec4f73dba24f2552ac28e73129375bcc7b6f
danvsn5 <danvsn5@gmail.com>
2024-05-13 20:44:56 +1200

    feat: improved styling for search box wrapper for smaller screens

--- src/components/browseMovies/SearchMovieCard.tsx
+++ src/components/browseMovies/SearchMovieCard.tsx
@@ -5,11 +5,9 @@ const SearchMovieCard = ({ movie }: any) => {
         <div className="search-card-result">
             <div className="thumbnail-and-title">
                 <ThumbnailBrowser movie={movie} />
-                <h1 className="search-card-title">
-                        {movie.title}
-                </h1>
             </div>
-                <h1 className="search-card-date">{movie.release_date.substring(0, 4)}</h1>
+            <h1 className="search-card-title">{movie.title}</h1>
+            <h1 className="search-card-date">{movie.release_date.substring(0, 4)}</h1>
         </div>
     )
 }
