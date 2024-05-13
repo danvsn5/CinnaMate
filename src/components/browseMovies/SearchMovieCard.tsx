279b356c38536edaae274445680a17c4f3e92f43
danvsn5 <danvsn5@gmail.com>
2024-05-13 21:00:54 +1200

    feat: implemented conditional rendering for search cards such that smaller screens do not render the thumbnail instance, allowing for the title text to be placed in the correct position

--- src/components/browseMovies/SearchMovieCard.tsx
+++ src/components/browseMovies/SearchMovieCard.tsx
@@ -1,13 +1,30 @@
 import ThumbnailBrowser from "./ThumbnailBrowser"
 
 const SearchMovieCard = ({ movie }: any) => {
+
+
     return (
         <div className="search-card-result">
-            <div className="thumbnail-and-title">
-                <ThumbnailBrowser movie={movie} />
-            </div>
-            <h1 className="search-card-title">{movie.title}</h1>
-            <h1 className="search-card-date">{movie.release_date.substring(0, 4)}</h1>
+
+            {window.innerWidth < 700 &&
+                <h1 className="search-card-title">{movie.title}</h1>
+            }
+            {window.innerWidth < 700 &&
+                <h1 className="search-card-date">{movie.release_date.substring(0, 4)}</h1>
+            }
+
+            {window.innerWidth >= 700 &&
+                <div className="thumbnail-and-title">
+                    <ThumbnailBrowser movie={movie} />
+                </div>
+            }
+            {window.innerWidth >= 700 &&
+                <h1 className="search-card-title">{movie.title}</h1>
+
+            }
+            {window.innerWidth >= 700 &&
+                <h1 className="search-card-date">{movie.release_date.substring(0, 4)}</h1>
+            }
         </div>
     )
 }
