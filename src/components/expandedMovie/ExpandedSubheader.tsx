57b7897928d6841befbfdbc7a18bc05215fe0fa5
danvsn5 <danvsn5@gmail.com>
2024-05-14 10:25:37 +1200

    feat: implemented conditional rendering for return button based on screen size and fixed visual bug with subheader titles

--- src/components/expandedMovie/ExpandedSubheader.tsx
+++ src/components/expandedMovie/ExpandedSubheader.tsx
@@ -8,10 +8,10 @@ function ExpandedSubheader(props: any) {
             <div className="subheader">
                 <div className="exp-subheader-title">
                     <h1> <span>&#x200B;</span> {props.subheaderTitle}</h1>
-                    {(PageID.pageIdentifier == "Home" && PageID.onMovie == true) && <Link to="/">
+                    {(PageID.pageIdentifier == "Home" && PageID.onMovie == true && window.innerWidth > 1000) && <Link to="/">
                         <button className="exp-return-button">Return</button>
                     </Link>}
-                    {(PageID.pageIdentifier == "Browse" && PageID.onMovie == true) && <Link to="/browser">
+                    {(PageID.pageIdentifier == "Browse" && PageID.onMovie == true && window.innerWidth > 1000) && <Link to="/browser">
                         <button className="exp-return-button">Return</button>
                     </Link>}
                 </div>
