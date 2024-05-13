cd7c240504b7275407632e938840ce1d95e6799e
danvsn5 <danvsn5@gmail.com>
2024-05-13 20:20:30 +1200

    feat: improved test cases to so that mobile devices are included in the tests

--- cypress/e2e/initialisation/browsing.cy.ts
+++ cypress/e2e/initialisation/browsing.cy.ts
@@ -91,10 +91,8 @@ describe('browse-page', () => {
     })
 
     it('check if the items from searching list are all the same height', () => {
-
         // searches for pl in input box
         cy.get('.search-input-editor').type("pl")
-
         cy.get('.search-card-title:first').then((element) => {
             // gets the height of the text in the first box, assuming no wrapping
             let height = element.height();
@@ -102,7 +100,55 @@ describe('browse-page', () => {
             // their thumbnail cards will be of the same size
             cy.get('.search-card-title:last').should('have.css', 'height', `${height}px`)
         })
+    })
 
+    it('check if the items from searching list are all the same height for phones', () => {
+        // searches for pl in input box
+        cy.viewport("samsung-note9")
+        cy.get('.search-input-editor').type("pl")
+        cy.get('.search-card-title:first').then((element) => {
+            // gets the height of the text in the first box, assuming no wrapping
+            let height = element.height();
+            // height of text in last box should be equal; if they are equal, then
+            // their thumbnail cards will be of the same size
+            cy.get('.search-card-title:last').should('have.css', 'height', `${height}px`)
+        })
+    })
+    it('check if the items from searching list are all the same height for tablets', () => {
+        // searches for pl in input box
+        cy.viewport("ipad-mini")
+        cy.get('.search-input-editor').type("pl")
+        cy.get('.search-card-title:first').then((element) => {
+            // gets the height of the text in the first box, assuming no wrapping
+            let height = element.height();
+            // height of text in last box should be equal; if they are equal, then
+            // their thumbnail cards will be of the same size
+            cy.get('.search-card-title:last').should('have.css', 'height', `${height}px`)
+        })
+    })
+    it('check if the items from searching list are all the same height for medium laptops', () => {
+        // searches for pl in input box
+        cy.viewport("macbook-11")
+        cy.get('.search-input-editor').type("pl")
+        cy.get('.search-card-title:first').then((element) => {
+            // gets the height of the text in the first box, assuming no wrapping
+            let height = element.height();
+            // height of text in last box should be equal; if they are equal, then
+            // their thumbnail cards will be of the same size
+            cy.get('.search-card-title:last').should('have.css', 'height', `${height}px`)
+        })
+    })
+    it('check if the items from searching list are all the same height small phones', () => {
+        // searches for pl in input box
+        cy.viewport("iphone-8")
+        cy.get('.search-input-editor').type("pl")
+        cy.get('.search-card-title:first').then((element) => {
+            // gets the height of the text in the first box, assuming no wrapping
+            let height = element.height();
+            // height of text in last box should be equal; if they are equal, then
+            // their thumbnail cards will be of the same size
+            cy.get('.search-card-title:last').should('have.css', 'height', `${height}px`)
+        })
     })
 
 })
