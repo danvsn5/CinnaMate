d25ff84b543aa2832ada90920761fcbe0562a43e
danvsn5 <danvsn5@gmail.com>
2024-05-17 15:18:16 +1200

    feat: implemented animation style for burger bar so that the user is not greeted to a jarring visual of the bar opening

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,13 +17,42 @@ const Header = () => {
     const [isburgerOpen, setBurger] = useState(false);
 
     function displayBurger() {
+        //sets variables for ID elements
         var burgerItems = document.getElementById("burger-list-ID");
+        var buttonA = document.getElementById("itm-btn-A");
+        var buttonB = document.getElementById("itm-btn-B");
+        var buttonC = document.getElementById("itm-btn-C");
 
         if (isburgerOpen == false) {
-            if (burgerItems) burgerItems.style.display = "block"
+            // if the burger bar is currently closed, open the bar by setting the constant height which functions
+            // through animation, set the display style to block and then set the opacity to 1 after the 
+            // burger bar has been opened with animation
+            if (burgerItems) burgerItems.style.height = "240px"
+            if (buttonA) buttonA.style.display = "block"
+            if (buttonB) buttonB.style.display = "block"
+            if (buttonC) buttonC.style.display = "block"
+
+            setTimeout(function () {
+                if (buttonA) buttonA.style.opacity = "1"
+                if (buttonB) buttonB.style.opacity = "1"
+                if (buttonC) buttonC.style.opacity = "1"
+            }, 350);
+
             setBurger(true);
         } else {
-            if (burgerItems) burgerItems.style.display = "none"
+            // closes buttons with fade out animation; once animation is complete, closes the rest of the 
+            // burger bar
+            if (buttonA) buttonA.style.opacity = "0"
+            if (buttonB) buttonB.style.opacity = "0"
+            if (buttonC) buttonC.style.opacity = "0"
+
+            setTimeout(function () {
+                if (burgerItems) burgerItems.style.height = "0px"
+                if (buttonA) buttonA.style.display = "none"
+                if (buttonB) buttonB.style.display = "none"
+                if (buttonC) buttonC.style.display = "none"
+            }, 350);
+
             setBurger(false)
         }
     }
@@ -81,23 +110,21 @@ const Header = () => {
                     <span />
                 )}
                 <ul className="list-items" id="burger-list-ID">
-
                     <li>
                         <Link to="/">
-                            <button className="burger-item-button" onClick={displayBurger}>Home</button>
+                            <button className="burger-item-button" id="itm-btn-A" onClick={displayBurger}>Home</button>
                         </Link>
                     </li>
                     <li>
                         <Link to="/browser">
-                            <button className="burger-item-button" onClick={displayBurger}>Browse Movies</button>
+                            <button className="burger-item-button" id="itm-btn-B" onClick={displayBurger}>Browse Movies</button>
                         </Link>
                     </li>
                     <li>
                         <Link to="/movies">
-                            <button className="burger-item-button" onClick={displayBurger}>My Movies</button>
+                            <button className="burger-item-button" id="itm-btn-C" onClick={displayBurger}>My Movies</button>
                         </Link>
                     </li>
-
                 </ul>
             </div>
         </header >
