8a73066b50e244f702a957db631992df3429ee85
danvsn5 <danvsn5@gmail.com>
2024-05-17 15:51:32 +1200

    refactor: refactored css hover effect into TS onclick listener for better mobile support

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,15 @@ import { useState, useEffect } from "react";
 
 const Header = () => {
 
+    var topB = document.getElementById("topB");
+    var topBA = document.getElementById("topBA");
+
+    var midB = document.getElementById("midB");
+    var midBA = document.getElementById("midBA");
+
+    var botB = document.getElementById("botB");
+    var botBA = document.getElementById("botBA");
+
     const [isDesktop, setDesktop] = useState(window.innerWidth < 956);
 
     const updateMedia = () => {
@@ -14,6 +23,40 @@ const Header = () => {
         return () => window.removeEventListener("resize", updateMedia);
     });
 
+    function dispBurgerStyle() {
+        if (topB) topB.style.width = "100%"
+        if (topBA) topBA.style.width = "100%"
+        if (midB) midB.style.width = "100%"
+        if (midBA) midBA.style.width = "100%"
+        if (botB) botB.style.width = "100%"
+        if (botBA) botBA.style.width = "100%"
+
+        if (topB) topB.style.background = "var(--light-purple)"
+        if (topBA) topBA.style.background = "var(--light-purple)"
+        if (midB) midB.style.background = "var(--light-purple)"
+        if (midBA) midBA.style.background = "var(--light-purple)"
+        if (botB) botB.style.background = "var(--light-purple)"
+        if (botBA) botBA.style.background = "var(--light-purple)"
+
+    }
+
+    function hideBurgerStyle() {
+        if (topB) topB.style.width = "35px"
+        if (topBA) topBA.style.width = "35px"
+        if (midB) midB.style.width = "30px"
+        if (midBA) midBA.style.width = "30px"
+        if (botB) botB.style.width = "35px"
+        if (botBA) botBA.style.width = "35px"
+
+        if (topB) topB.style.background = "#ffffff"
+        if (topBA) topBA.style.background = "#ffffff"
+        if (midB) midB.style.background = "#ffffff"
+        if (midBA) midBA.style.background = "#ffffff"
+        if (botB) botB.style.background = "#ffffff"
+        if (botBA) botBA.style.background = "#ffffff"
+
+    }
+
     const [isburgerOpen, setBurger] = useState(false);
 
     function displayBurger() {
@@ -23,10 +66,15 @@ const Header = () => {
         var buttonB = document.getElementById("itm-btn-B");
         var buttonC = document.getElementById("itm-btn-C");
 
+
+
         if (isburgerOpen == false) {
             // if the burger bar is currently closed, open the bar by setting the constant height which functions
             // through animation, set the display style to block and then set the opacity to 1 after the 
             // burger bar has been opened with animation
+
+            dispBurgerStyle()
+
             if (burgerItems) burgerItems.style.height = "240px"
             if (buttonA) buttonA.style.display = "block"
             if (buttonB) buttonB.style.display = "block"
@@ -42,6 +90,9 @@ const Header = () => {
         } else {
             // closes buttons with fade out animation; once animation is complete, closes the rest of the 
             // burger bar
+            hideBurgerStyle()
+
+
             if (buttonA) buttonA.style.opacity = "0"
             if (buttonB) buttonB.style.opacity = "0"
             if (buttonC) buttonC.style.opacity = "0"
@@ -93,16 +144,16 @@ const Header = () => {
                     <div className="burger-bar">
                         <div className="hamburger-icon-A">
                             <button className="burger-button-A" onClick={displayBurger}>
-                                <span className="top-burger left" />
-                                <span className="mid-burger left" />
-                                <span className="bot-burger left" />
+                                <span className="top-burger left" id="topB" />
+                                <span className="mid-burger left" id="midB" />
+                                <span className="bot-burger left" id="botB" />
                             </button>
                         </div>
                         <div className="hamburger-icon-B">
                             <button className="burger-button-B" onClick={displayBurger}>
-                                <span className="top-burger right" />
-                                <span className="mid-burger right" />
-                                <span className="bot-burger right" />
+                                <span className="top-burger right" id="topBA" />
+                                <span className="mid-burger right" id="midBA" />
+                                <span className="bot-burger right" id="botBA" />
                             </button>
                         </div>
                     </div>
