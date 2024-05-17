3e93adf42ad9bf7f822d0c13ac0a2908b4f97d61
danvsn5 <danvsn5@gmail.com>
2024-05-18 10:02:28 +1200

    test: created new test case to test full mobile navigation functionality between many pages with functionality involved

---
+++ cypress/e2e/mobile-e2e.cy.ts
@@ -0,0 +1,49 @@
+describe('mobile e2e with full navigation', () => {
+    beforeEach(() => {
+
+        // opens up build webpage 
+        cy.visit('http://localhost:4173/')
+
+    })
+
+    it('Full test for small phone screen', () => {
+
+        // sets the viewport to a large sized phone
+        cy.viewport('iphone-8')
+
+        // open the burger menu
+        cy.get('.burger-button-A').click()
+        cy.get('.burger-button-B').click()
+
+        // open the browse page menu
+        cy.get('#itm-btn-B').click()
+
+        // searches for dune; checks if there are three search card results; checks if the 
+        // search button works and can find the first page element; clicks on element
+        cy.get('.search-input-editor').type("dune")
+        cy.get('.search-card-result').should('have.length', 3)
+        cy.get('.search-input-button').click()
+        cy.get('.actual-thumb:first').click()
+
+        // check if the correct page is opened and the necessary items are visible for validation
+        cy.get('.exp-subheader-title').should('be.visible')
+        cy.get('.actual-expanded-thumb').should('be.visible')
+        
+        // returns back to the home page and clicks on the first page card 
+        cy.get('.burger-button-A').click()
+        cy.get('#itm-btn-A').click()
+        cy.get('.actual-thumb:first').click()
+
+        // checks if the correct items are visible in the expanded card page
+        cy.get('.exp-subheader-title').should('be.visible')
+        cy.get('.actual-expanded-thumb').should('be.visible')
+
+        // returns back to the home page 
+        cy.get('.burger-button-A').click()
+        cy.get('#itm-btn-A').click()
+
+        // checks if there are 20 cards in the trending page as expected
+        cy.get('.page-card').should('have.length', 20)
+
+    })
+})
