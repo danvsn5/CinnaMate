77e12437ac6d6b3d282701626d9659b0732b8d0c
danvsn5 <danvsn5@gmail.com>
2024-05-31 12:38:17 +1200

    fixed import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './components/utils/PageVariables.tsx'
+import './components/utils/PageVariables.ts'
 
 /* ————————————————————————————————————————————— CSS ———————————————————————————————————————————— */
 
