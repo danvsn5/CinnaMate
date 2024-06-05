f3eead4c7099dafc6b090e50d099c82ab5f3f8c5
danvsn5 <danvsn5@gmail.com>
2024-06-06 11:15:21 +1200

    feat: implemented onclick button functionality to swap the displayed content based on which button is active

--- src/components/myMoviesTab/Tabs.tsx
+++ src/components/myMoviesTab/Tabs.tsx
@@ -1,23 +1,39 @@
+import { useState } from "react"
 import MovieList from "../utils/collectMovies"
 
 const Tabs = () => {
+
+    const [currentTabContent, setTabContent] = useState("favourites")
+
+    function setFavourites() {
+        setTabContent("favourites")
+    }
+
+    function setSeen() {
+        setTabContent("seen")
+    }
+
+    function setWatchlist() {
+        setTabContent("watchlist")
+    }
+
     return (
         <div>
             <div className="tabs-container">
                 <ul className="tabs-list">
                     <li className="tab-list-item">
-                        <button className="movie-tab-button active-tab">Favourites</button>
+                        <button id="fav-tab" className="movie-tab-button active-tab" onClick={setFavourites}>Favourites</button>
                     </li>
                     <li className="tab-list-item">
-                        <button className="movie-tab-button">Seen</button>
+                        <button id="seen-tab" className="movie-tab-button" onClick={setSeen}>Seen</button>
                     </li>
                     <li className="tab-list-item">
-                        <button className="movie-tab-button">Watchlist</button>
+                        <button id="watchlist-tab" className="movie-tab-button" onClick={setWatchlist}>Watchlist</button>
                     </li>
                 </ul>
             </div>
             <hr className="browsing-break" />
-            <MovieList dbCategory="favourites"/>
+            <MovieList dbCategory={currentTabContent} />
         </div>
 
 
