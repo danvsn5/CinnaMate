c23ddd733df5183e5cf46658e3c38b0bea6db815
danvsn5 <danvsn5@gmail.com>
2024-06-08 10:04:50 +1200

    feat: added to test suite by testing exp movie button functionality

--- cypress/e2e/sign-up-db.cy.ts
+++ cypress/e2e/sign-up-db.cy.ts
@@ -32,7 +32,8 @@ describe('full db test on desktop', () => {
         // clicks off the modal overlay by waiting for any animations to occur and then
         // clicking off the modal in order to close it
 
-        cy.wait(1000)
+        cy.wait(2000)
+
         cy.get('.ReactModal__Overlay:last').click('topLeft')
 
         cy.get('.fav-button:first .icon').should('have.css', 'color', 'rgb(255, 255, 255)')
@@ -81,7 +82,7 @@ describe('full db test on desktop', () => {
         // submits
         cy.get('.submit').click()
 
-        cy.wait(1000)
+        cy.wait(2000)
         cy.get('.ReactModal__Overlay:last').click('topLeft')
 
         // checks if items are the correct color based on whether or not they're in the database
@@ -102,4 +103,54 @@ describe('full db test on desktop', () => {
 
 
     })
+
+
+    it('Exp movie css test', () => {
+
+        deleteDoc(doc(db, "users", "danvsnTEST"));
+
+        // clicks the sign up button; modal should appear
+        cy.get('.sign-up-button').click()
+
+        // types username to sign up
+        cy.get('.sign-up-user').type("danvsnTEST")
+
+        // types password to sign up 
+        cy.get('.sign-up-password').type("TEST")
+
+        // submits
+        cy.get('.submit').click()
+
+        // clicks off the modal overlay by waiting for any animations to occur and then
+        // clicking off the modal in order to close it
+
+        cy.wait(10000)
+        cy.get('.ReactModal__Overlay:first').click('topLeft')
+
+        // checks if the first fav icon is colored correctly since it is not inside the database
+        cy.get('.fav-button:first .icon').should('have.css', 'color', 'rgb(255, 255, 255)')
+
+        // clicks on the first thumbnail
+        cy.get('.actual-thumb:first').click()
+
+        cy.wait(4000)
+
+
+        cy.get('.exp-fav-button .icon').should('have.css', 'color', 'rgb(255, 255, 255)')
+
+        cy.get('.exp-fav-button').click()
+
+        // after clicking, the icon should have changed the color
+        cy.get('.exp-fav-button .icon').should('have.css', 'color', 'rgb(90, 0, 154)')
+
+        // returns to the home page
+        cy.get('.exp-return-button').click()
+
+        // checks if the first fav icon is colored correctly since it is currently inside the database
+        cy.get('.fav-button:first .icon').should('have.css', 'color', 'rgb(90, 0, 154)')
+
+        deleteDoc(doc(db, "users", "danvsnTEST"));
+
+    })
+
 })
