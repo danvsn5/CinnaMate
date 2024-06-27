b05a0d2d5c802f977f040e69417c9cb387029abd
danvsn5 <danvsn5@gmail.com>
2024-06-28 11:08:33 +1200

    fix: added catch to ensure that empty tags are not added to the database

--- src/components/expandedMovie/MovieTag.tsx
+++ src/components/expandedMovie/MovieTag.tsx
@@ -49,18 +49,24 @@ const MovieTag = ({ movieID }: any) => {
                 }
             };
 
-            await setDoc(userDocRef, data, { merge: true });
-        }
-    };
+            // if either the tag title or tag content is empty, the tag will not be added to the database
+            if (tagTitle === "" || tagContent === "") {
+                return
+            }
+            else {
+                await setDoc(userDocRef, data, { merge: true });
+            }
+        };
+    }
 
     return (
         <div className='movie-tag'>
             <hr className="break-tag" />
             <h1 className='main-tagline tag'>Create Tag</h1>
             <h1 className="text-tag-label">Title and/or Timestamp</h1>
-            <textarea className="editor tag-title" rows={1} spellCheck={false} wrap="soft" placeholder="Write the title and/or timestamp of your tag..." onChange={inputTitleChange}/>
+            <textarea className="editor tag-title" rows={1} spellCheck={false} wrap="soft" placeholder="Write the title and/or timestamp of your tag..." onChange={inputTitleChange} />
             <h1 className="text-tag-label">Tag Content</h1>
-            <textarea className="editor tag-content" spellCheck={false} rows={5} maxLength={550} placeholder="Write your tag content..." onChange={inputContentChange}/>
+            <textarea className="editor tag-content" spellCheck={false} rows={5} maxLength={550} placeholder="Write your tag content..." onChange={inputContentChange} />
             <button className='exp-thumb-button add-tag-button' data-tooltip-id="my-tooltip3" data-tooltip-content="Add Tag to Taglist" onClick={handleClick}>
                 <Tooltip id='my-tooltip3' className='exp-tooltip' border="1px solid var(--light-purple)" delayShow={250} place='bottom' offset={30} arrowColor='var(--light-purple )' />
                 <i className="icon fa-solid fa-plus exp-icon"></i>
