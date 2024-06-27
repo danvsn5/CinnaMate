a3f21a82ddec46801c9359f46850206837fdf9ee
danvsn5 <danvsn5@gmail.com>
2024-06-28 11:05:56 +1200

    feat: implemented both tag title and content that gets added to the database

--- src/components/expandedMovie/MovieTag.tsx
+++ src/components/expandedMovie/MovieTag.tsx
@@ -8,14 +8,17 @@ const MovieTag = ({ movieID }: any) => {
     movieID = movieID;
 
     const [tagTitle, setTitle] = useState("")
+    const [tagContent, setContent] = useState("")
 
     const inputTitleChange = (e: any) => {
         setTitle(e.currentTarget.value)
     }
+    const inputContentChange = (e: any) => {
+        setContent(e.currentTarget.value)
+    }
 
     // upon clicking the add tag button, the database is updated with the contents from the tag title and the tag content
     // tags element in database will contain a movie id, and all associated tags with their titles and content
-
     const handleClick = async () => {
         if (loggedInState.isLoggedIn) {
             const userDocRef = doc(db, "users", loggedInState.username);
@@ -29,9 +32,11 @@ const MovieTag = ({ movieID }: any) => {
             }
 
             const movieTags = existingTags[movieID] || [];
-            if (!movieTags.includes(tagTitle)) {
-                movieTags.push(tagTitle);
-            }
+            const newTag = {
+                title: tagTitle,
+                content: tagContent
+            };
+            movieTags.push(newTag);
 
             const updatedTags = {
                 ...existingTags,
@@ -53,9 +58,9 @@ const MovieTag = ({ movieID }: any) => {
             <hr className="break-tag" />
             <h1 className='main-tagline tag'>Create Tag</h1>
             <h1 className="text-tag-label">Title and/or Timestamp</h1>
-            <textarea className="editor tag-title" rows={1} spellCheck={false} wrap="soft" placeholder="Write the title and/or timestamp of your tag..." onChange={inputTitleChange}></textarea>
+            <textarea className="editor tag-title" rows={1} spellCheck={false} wrap="soft" placeholder="Write the title and/or timestamp of your tag..." onChange={inputTitleChange}/>
             <h1 className="text-tag-label">Tag Content</h1>
-            <textarea className="editor tag-content" spellCheck={false} rows={5} maxLength={550} placeholder="Write your tag content..."></textarea>
+            <textarea className="editor tag-content" spellCheck={false} rows={5} maxLength={550} placeholder="Write your tag content..." onChange={inputContentChange}/>
             <button className='exp-thumb-button add-tag-button' data-tooltip-id="my-tooltip3" data-tooltip-content="Add Tag to Taglist" onClick={handleClick}>
                 <Tooltip id='my-tooltip3' className='exp-tooltip' border="1px solid var(--light-purple)" delayShow={250} place='bottom' offset={30} arrowColor='var(--light-purple )' />
                 <i className="icon fa-solid fa-plus exp-icon"></i>
